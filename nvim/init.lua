48c554e94b0ca4093f95e08e41d31c5986f59271
YHiroyuki <sample@hoge.com>
2024-05-24 09:34:15 +0900

    WIP

--- nvim/init.lua
+++ nvim/init.lua
@@ -67,3 +67,101 @@ vim.api.nvim_create_user_command(
   end,
   { desc = "Format the current buffer using LSP" }
 )
+
+
+-- Lua関数の定義
+local api = vim.api
+
+function create_floating_window()
+  local row, col = unpack(api.nvim_win_get_cursor(0))
+  local width = 40
+  local height = 2
+  local opts = {
+    relative = 'editor',
+    -- anchor = 'NW',
+    width = width,
+    height = height,
+    row = row - 1 - (height + 2),
+    col = 0,
+    style = 'minimal',
+    border = 'rounded'
+  }
+  local target_path = "/home/yhiroyuki/sample/aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa/"
+  local buf = api.nvim_create_buf(false, true)
+  local win = api.nvim_open_win(buf, true, opts)
+
+  -- バッファで折り返しを無効にする
+  api.nvim_win_set_option(win, 'wrap', false)
+
+  api.nvim_buf_set_lines(buf, 0, -1, false, { "Create File or Directory:", target_path })
+
+  api.nvim_buf_set_keymap(buf, 'i', '<CR>', '', {
+    noremap = true,
+    silent = true,
+    callback = function()
+      -- TODO: ここにディレクトリ作成の処理を書く
+      api.nvim_win_close(win, true)
+    end
+  })
+
+  -- ウィンドウでインサートモードを有効にし、テキストの末尾から入力開始
+  api.nvim_win_set_cursor(win, { height, string.len(target_path) + 1 }) -- カーソルを最後の行の最後に移動
+  vim.api.nvim_input('a')
+end
+
+vim.api.nvim_create_user_command('FloatingWindow', create_floating_window, {})
+
+-- ターミナルウィンドウが開かれたときの設定
+vim.api.nvim_create_autocmd('TermOpen', {
+  pattern = '*',
+  callback = function()
+    vim.wo.number = false
+    vim.wo.relativenumber = false
+  end
+})
+
+
+local function open_terminal()
+  local term_bufnr = nil
+  -- 現在のバッファのリストを取得
+  for _, bufnr in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.bo[bufnr].buftype == 'terminal' then
+      term_bufnr = bufnr
+      break
+    end
+  end
+
+  if term_bufnr then
+    -- 既存のターミナルバッファを開く
+    vim.cmd('buffer ' .. term_bufnr)
+  else
+    -- 新しいターミナルバッファを作成
+    vim.cmd('terminal')
+  end
+end
+
+vim.api.nvim_create_user_command('Terminal', open_terminal, {})
+
+
+-- ダブルバイトスペースを強調表示
+local function DoubleByteSpace()
+  vim.api.nvim_set_hl(0, 'DoubleByteSpace', { bg = 'darkgray' })
+end
+
+if vim.fn.has('syntax') == 1 then
+  vim.api.nvim_create_augroup('DoubleByteSpace', { clear = true })
+
+  vim.api.nvim_create_autocmd({ 'ColorScheme' }, {
+    group = 'DoubleByteSpace',
+    callback = DoubleByteSpace,
+  })
+
+  vim.api.nvim_create_autocmd({ 'VimEnter', 'WinEnter', 'BufRead' }, {
+    group = 'DoubleByteSpace',
+    callback = function()
+      vim.fn.matchadd('DoubleByteSpace', '　')
+    end,
+  })
+
+  DoubleByteSpace()
+end
--- nvim/lua/plugin_settings.lua
+++ nvim/lua/plugin_settings.lua
@@ -226,7 +226,7 @@ local function tree()
 
     -- custom mappings
     -- vim.keymap.set('n', '<C-t>', api.tree.change_root_to_parent, opts('Up'))
-    vim.keymap.set('n', 'n', api.fs.create, opts('Create File Or Directory'))
+    vim.keymap.set('n', 't', api.fs.create, opts('Create File Or Directory'))
   end
 
 
@@ -304,6 +304,32 @@ local function tree()
   require('keymap').tree()
 end
 
+local function noice()
+  local status, noice = pcall(require, "noice")
+  if not status then
+    return
+  end
+
+  noice.setup({
+    -- lsp = {
+    --   -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    --   override = {
+    --     ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+    --     ["vim.lsp.util.stylize_markdown"] = true,
+    --     ["cmp.entry.get_documentation"] = true,
+    --   },
+    -- },
+    -- you can enable a preset for easier configuration
+    presets = {
+      bottom_search = false,      -- use a classic bottom cmdline for search
+      command_palette = false,    -- position the cmdline and popupmenu together
+      long_message_to_split = true, -- long messages will be sent to a split
+      inc_rename = false,         -- enables an input dialog for inc-rename.nvim
+      lsp_doc_border = false,     -- add a border to hover docs and signature help
+    },
+  })
+end
+
 M = {
   fzf = fzf,
   ddc = ddc,
@@ -311,6 +337,7 @@ M = {
   lexima = lexima,
   lualine = lualine,
   tree = tree,
+  noice = noice,
   get_ddu_cr_action = get_ddu_cr_action,
 }
 
